677473543fac21c66f4bbc837690c6263752cbdd
hhyowon <hyowon0605@gmail.com>
2023-08-28 18:23:04 +0900

    0828

--- codes/MLs/Classifications/BreastCancerWisconsin_quest.py
+++ codes/MLs/Classifications/BreastCancerWisconsin_quest.py
@@ -4,9 +4,9 @@ area_se = float(input('area_se: '))
 
 import pickle
 
-with open('datasets/BreastCancerWisconsin_Regression.pki','rb') as regression_file :
+with open('datasets/BreastCancerWisconsinDataSet.pki','rb') as regression_file :
     loaded_model = pickle.load(regression_file)
     input_labels = [[radius_se, texture_se, area_se]]  # 학습했던 설명변수 형식 
     result_predict = loaded_model.predict(input_labels)
-    print('Predict radius_mean Result : {}'.format(result_predict))
+    print('Predict Result : {}'.format(result_predict))
     pass
